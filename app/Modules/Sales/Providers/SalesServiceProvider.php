7df59f7c114680da9e00edc3a4fb9c525ceccb16
portAmmar <ammar.k.alhussary@proton.me>
2026-07-12 17:19:03 +0300

    add Inventory Service Provider

--- app/Modules/Sales/Providers/SalesServiceProvider.php
+++ app/Modules/Sales/Providers/SalesServiceProvider.php
@@ -1,3 +1,35 @@
 <?php
 
-namespace Modules\Sales;
+namespace Modules\Sales\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class  namespace Modules\Inventory\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class  InventoryServiceProvider extends ServiceProvider
+{
+
+    public function register()
+    {
+        parent::register();
+    }
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+    }
+} extends ServiceProvider
+{
+
+    public function register()
+    {
+        parent::register();
+    }
+    public function boot()
+    {
+        $this->loadRoutesFrom(__DIR__ . '/../routes.php');
+        $this->loadMigrationsFrom(__DIR__ . '/../Database/Migrations');
+    }
+}
