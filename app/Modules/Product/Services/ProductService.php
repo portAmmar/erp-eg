463efc8cd2f1946780fc23c9aea02a267cbc7add
portAmmar <ammar.k.alhussary@proton.me>
2026-08-04 23:37:41 +0300

    feat(product): create create f(x)

--- app/Modules/Product/Services/ProductService.php
+++ app/Modules/Product/Services/ProductService.php
@@ -2,11 +2,9 @@
 
 namespace Modules\Product\Services;
 use Modules\Product\Contracts\ProductInterface;
+
 class ProductService implements ProductInterface
 {
-    public function create(){}
-    public function update(){}
-    public function find(){}
-    public function findBySKU(){}
-    public function delete(){}
+    public function Create(){
+    };
 }
